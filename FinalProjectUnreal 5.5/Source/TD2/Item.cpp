e726d9a3af906a4e0ac57a4d336f4bb9155dc5c3
MohanedB <90917177+MohanedB@users.noreply.github.com>
2025-04-17 20:17:30 -0400

    Got the Items Working
    Got the Items Working

--- FinalProjectUnreal 5.5/Source/TD2/Item.cpp
+++ FinalProjectUnreal 5.5/Source/TD2/Item.cpp
@@ -8,47 +8,53 @@
 // Sets default values
 AItem::AItem()
 {
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LaserMesh"));
-	RootComponent = Mesh;
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+    RootComponent = Mesh;
 
-	Mesh->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnOverLapBegin);
+    Mesh->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnOverLapBegin);
 }
 
 // Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
 }
 
 void AItem::OnOverLapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Log, TEXT("Enter item"));
-	if (OtherActor)
-	{
-		ATP_ThirdPersonCharacter* Character = Cast<ATP_ThirdPersonCharacter>(OtherActor);
-		if (Character)
-		{
-			if (UWorld* World = GetWorld())
-			{
-				if (ATP_ThirdPersonGameMode* GameMode = Cast<ATP_ThirdPersonGameMode>(World->GetAuthGameMode()))
-				{
-					GameMode->IncrementScore();
-					//Destroy();
-					UE_LOG(LogTemp, Log, TEXT("Hide item"));
-					SetActorHiddenInGame(true);
-					SetActorEnableCollision(false);
-				}
-			}
-		}
-	}
+    UE_LOG(LogTemp, Log, TEXT("Enter item"));
+    if (OtherActor)
+    {
+        ATP_ThirdPersonCharacter* Character = Cast<ATP_ThirdPersonCharacter>(OtherActor);
+        if (Character)
+        {
+
+            OnCollected();
+
+            SetActorHiddenInGame(true);
+            SetActorEnableCollision(false);
+
+            /*
+            if (UWorld* World = GetWorld())
+            {
+                if(ATP_ThirdPersonGameMode* GameMode = Cast<ATP_ThirdPersonGameMode>(World->GetAuthGameMode()))
+                {
+                    GameMode->IncrementScore();
+
+                    SetActorHiddenInGame(true);
+                    SetActorEnableCollision(false);
+                }
+            }
+            */
+        }
+    }
 }
 
 void AItem::RespawnItem()
 {
-	UE_LOG(LogTemp, Log, TEXT("Respawn item"));
+    UE_LOG(LogTemp, Log, TEXT("Respawn item"));
 
-	SetActorHiddenInGame(false);
-	SetActorEnableCollision(true);
+    SetActorHiddenInGame(false);
+    SetActorEnableCollision(true);
 }
-
